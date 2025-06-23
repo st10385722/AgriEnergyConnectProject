f635db2430e92d1d2fe4e98247a345d233a610b0
Tyron Jeremiah Naidoo <st10385722@vcconnect.edu.za>
2025-06-23 02:05:10 +0200

    Added readme and sql file

--- obj/Debug/net9.0/Agri-EnergyConnect.AssemblyInfo.cs
+++ obj/Debug/net9.0/Agri-EnergyConnect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agri-EnergyConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4d288e61e8bb68429a5d8fbce04299c6777d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e11d83c134a3e921d335cc4b5b59852bbed18ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agri-EnergyConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agri-EnergyConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
